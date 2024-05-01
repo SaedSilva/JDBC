67510b1f24c5375e4e7387b0e0f6894630827f37
Saed Silva Sousa <saed.sousa@hotmail.com>
2024-05-01 13:58:27 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/ProgramSeller.java
+++ src/application/ProgramSeller.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class ProgramSeller {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         Seller seller = new Seller(1, "Saed", "saed@gmail.com", new Date(), 2000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
         System.out.println(department);
