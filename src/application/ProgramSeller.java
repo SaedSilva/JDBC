a0e0dbec8dc2bd9477844f8e0465e7493a48e3e2
Saed Silva Sousa <saed.sousa@hotmail.com>
2024-05-01 21:52:21 -0300

    Reusing instantiation

--- src/application/ProgramSeller.java
+++ src/application/ProgramSeller.java
@@ -1,17 +1,14 @@
 package application;
 
-import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class ProgramSeller {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("---Teste #1: Seller FindByID---");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
