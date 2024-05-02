775c8bdb02c5d8089de7636e8df9c793b232c8c2
Saed Silva Sousa <saed.sousa@hotmail.com>
2024-05-01 22:11:42 -0300

    findAll implementation

--- src/application/ProgramSeller.java
+++ src/application/ProgramSeller.java
@@ -14,11 +14,17 @@ public class ProgramSeller {
         System.out.println("---Teste #1: Seller FindByID---");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        System.out.println();
 
         System.out.println("---Teste #2: Seller FindByDepartment---");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         list.forEach(System.out::println);
+        System.out.println();
 
+        System.out.println("---Teste #3: Seller FindAll---");
+        list = sellerDao.findAll();
+        list.forEach(System.out::println);
+        System.out.println();
     }
 }
